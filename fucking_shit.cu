c165e7267efb62ece606f0189b9aeec0391d6132
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2020-05-05 19:42:36 +0300

    Docstrings and renames in fucking_shit.*

--- fucking_shit.cu
+++ fucking_shit.cu
@@ -6,20 +6,6 @@
 namespace jc = jones_constants;
 
 
-__device__ void diffuse_trail(MapNode *node)
-{
-    auto get_trail = [](MapNode *m)
-    { return m->trail; };
-
-    ll sum = get_trail(node) + get_trail(node->top->left) +
-             get_trail(node->top) + get_trail(node->top->right) +
-             get_trail(node->left) + get_trail(node->right) +
-             get_trail(node->bottom->left) + get_trail(node->bottom) +
-             get_trail(node->bottom->right);
-
-    node->temp_trail = (double) sum / (1 - jc::diffdamp);
-}
-
 __device__ void create_particle(MapNode *node)
 {
     /* Please, note that we're using `new` and `delete` operators for allocating and deallocating Particles,
@@ -41,7 +27,22 @@ __device__ void delete_particle(MapNode *node)
 }
 
 
-__device__ ll get_particle_window(MapNode *node, int window_size)
+__device__ void diffuse_trail(MapNode *node)
+{
+    auto get_trail = [](MapNode *m)
+    { return m->trail; };
+
+    double sum = get_trail(node) + get_trail(node->top->left) +
+             get_trail(node->top) + get_trail(node->top->right) +
+             get_trail(node->left) + get_trail(node->right) +
+             get_trail(node->bottom->left) + get_trail(node->bottom) +
+             get_trail(node->bottom->right);
+
+    node->temp_trail = (1 - jc::diffdamp) * (sum / 9.0);
+}
+
+
+__device__ ll count_particles_in_node_window(MapNode *node, int window_size)
 {
     to_be_rewritten; // TODO: Rewrite this function in a more efficient way
 
@@ -75,8 +76,8 @@ __device__ void random_death_test(MapNode *node)
 
 __device__ void death_test(MapNode *node)
 {
-    ll particle_window = get_particle_window(node, jc::sw);
-    if(jc::smin <= particle_window && particle_window <= jc::smax)
+    ll particles_in_window = count_particles_in_node_window(node, jc::sw);
+    if(jc::smin <= particles_in_window && particles_in_window <= jc::smax)
     {/* if in survival range, then stay alive */}
     else
         delete_particle(node);
@@ -84,7 +85,7 @@ __device__ void death_test(MapNode *node)
 
 __device__ void division_test(MapNode *node)
 {
-    ll particle_window = get_particle_window(node, jc::gw);
+    ll particle_window = count_particles_in_node_window(node, jc::gw);
     if(jc::gmin <= particle_window && particle_window <= jc::gmax)
     {
         if(rand0to1() <= jc::division_probability)
--- fucking_shit.cuh
+++ fucking_shit.cuh
@@ -18,7 +18,7 @@
  * @param node The node to create particle at
  *
  * @warning This function must not be called if the node contains a particle already, calling it with a node
- * containing a particle already causes a memory leak
+ * containing a particle causes a memory leak
  */
 __device__ void create_particle(MapNode *node);
 
@@ -28,60 +28,82 @@ __device__ void create_particle(MapNode *node);
  * @param node The node to remove particle from
  *
  * @warning This function must only be called if the node contains a particle already, calling it with node
- * not containing a particle already causes undefined behaviour
+ * not containing a particle causes undefined behaviour
  */
 __device__ void delete_particle(MapNode *node);
 
-// TODO: add detailed description to the following docstring
 /**
- * Diffuses trail in the current node
+ * Diffuses trail in the given node
  *
- * <detailed description will appear soon>
+ * The diffusion algorithm (developed by Jeff Jones) is pretty simple at first sight. We calculate an average
+ * `trail` value in a 3x3 node window around the given one and multiply it by `(1 - jones_constants::diffdamp)`.
+ * The new `temp_trail` value in the given node is the value just calculated. This is a natural way to implement the
+ * smell spread: on each iteration smell moves more far from the source, but becomes less strong, because
+ * `(1 - jones_constants::diffdamp)` < 1
  *
- * @param node The node tu diffuse trail at
+ * @param node The node to diffuse trail at
  */
 __device__ void diffuse_trail(MapNode *node);
 
-// TODO: add detailed description to the following docstring
 /**
- * Returns number of particles in a particle window around the given node
+ * Returns number of particles in a node window around the given node
  *
- * <detailed description will appear soon>
+ * Let's assume a node of the model is a plane point with coordinates (X, Y). Then it's neighbors (even if they are not
+ * equidistant) will have (X-1, Y-1), (X-1, Y), (X-1, Y+1), (X, Y-1), us, (X, Y+1), (X+1, Y-1), (X+1, Y), (X+1, Y+1).
+ * Obviously, we can extrapolate this to neighbors of neighbors and so on. Then a node window around a node
+ * with (X, Y) "coordinates" of size W (odd integer) is a set of nodes in the square with side W and central
+ * point (X, Y). More formally a node window around (X, Y) node of size W is a set of nodes in a square with opposite
+ * vertices (X - (W-1)/2, Y - (W-1)/2) and (X + (W-1)/2, Y + (W-1)/2). <br><br>
+ * This function returns a number of particles in a node window around the given node of the given size
  *
- * @param node The node to get particle window around
- * @param window_size The particle window size
+ * @param node The node to get node window around
+ * @param window_size The node window size
  *
- * @return The number of particles in the particle window
+ * @note `window_size` must be a positive odd integer
+ *
+ * @returns The number of particles in the node window
  */
-__device__ ll get_particle_window(MapNode *node, int window_size);
+__device__ ll count_particles_in_node_window(MapNode *node, int window_size);
 
-// TODO: add detailed description to the following docstring
 /**
  * Runs a random death test in the given node
  *
- * <detailed description will appear soon>
+ * Removes a particle in the given node with the `jones_constants::death_random_probability` probability
  *
  * @param node The node to run random death test in
+ *
+ * @warning This function must only be called if the node contains a particle already, calling it with node
+ * not containing a particle causes undefined behaviour
  */
 __device__ void random_death_test(MapNode *node);
 
-// TODO: add detailed description to the following docstring
 /**
  * Runs a death test in the given node
  *
- * <detailed description will appear soon>
+ * Let's assume P is a number of particles in a node window around the given node of size `jones_constants::sw`. If
+ * `jones_constants::smin` <= P <= `jones_constants::smax`, then nothing happens. Otherwise the particle is removed
+ * from the given node
  *
  * @param node The node to run death test in
+ *
+ * @warning This function must only be called if the node contains a particle already, calling it with node
+ * not containing a particle causes undefined behaviour
  */
 __device__ void death_test(MapNode *node);
 
-// TODO: add detailed description to the following docstring
 /**
  * Runs a division test in the given node
  *
- * <detailed description will appear soon>
+ * Let's assume P is a number of particles in a node window around the given node of size `jones_constants::gw`. If
+ * `jones_constants::gmin` <= P <= `jones_constants::gmax` expression is FALSE, nothing happens. Otherwise (if the
+ * expression is true) the division process begins with the `jones_constants::division_probability` probability. In the
+ * division process we try to find a node without particle in a 3x3 node window around the given node. If we succeed,
+ * we create a particle in the found node. Otherwise nothing happens
  *
  * @param node The node to run division test in
+ *
+ * @warning This function must only be called if the node contains a particle already, calling it with node
+ * not containing a particle causes undefined behaviour
  */
 __device__ void division_test(MapNode *node);
 
