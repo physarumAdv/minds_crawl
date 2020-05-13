e8672f5ba9887f2208b0d9c61242f33faf8d7c01
tanya-kta <tanya-kta@bk.ru>
2020-05-14 00:14:10 +0300

    Added initialization of particle and point rotation

--- src/Particle.cu
+++ src/Particle.cu
@@ -1,1 +1,38 @@
 #include "Particle.cuh"
+#include "jones_constants.hpp"
+
+namespace jc = jones_constants;
+SpacePoint origin = {0, 0, 0};
+
+__device__ Particle::Particle(const Polyhedron *const polyhedron, int polyhedron_face,
+                              SpacePoint coordinates, int angle)
+{
+    this->coordinates = coordinates;
+    this->polyhedron_face = polyhedron_face;
+
+    Face current_face = polyhedron.get_face(polyhedron_face);
+    this->normal = current_face.normal;
+
+    SpacePoint radius = polyhedron.get_vertex(current_face.vertices[0]) - coordinates;
+    radius = radius * jc::so / get_distance(radius, origin);
+    this->miggle_sensor = this.rotate_point_angle(radius, angle);
+}
+
+__device__ SpacePoint Particle::rotate_point_angle(SpacePoint radius, int angle)
+{
+    double angle_cos = cos(angle);
+    return (1 - angle_cos) * (this->normal * radius) * this->normal + angle_cos * radius +
+                      sin(angle) * (this->normal % radius) + this->coordinates;
+}
+
+__device__ void Particle::init_left_right_sensors()
+{
+    this->left_sensor = this.rotate_point_angle(this->middle_sensor - this->coordinates, jc::sa);
+    this->right_sensor = this.rotate_point_angle(this->middle_sensor - this->coordinates, -jc::sa);
+    if(this->normal * ((this->right_sensor - this->coordinates) % (this->left_sensor - this->coordinates)) < 0)
+    {
+        SpacePoint p = this->right_sensor;
+        this->right_sensor = this->left_sensor;
+        this->left_sensor = p;
+    }
+}
--- src/Particle.cuh
+++ src/Particle.cuh
@@ -2,10 +2,10 @@
 #define MIND_S_CRAWL_PARTICLE_CUH
 
 #include "Polyhedron.cuh"
-#include "SpacePoint.hpp"
+#include "SpacePoint.cuh"
 
 
-/// Object describing a particle in the model
+/// Object describing a particle in the model (also called "agent" - from the original Jones' book)
 class Particle
 {
 public:
@@ -15,8 +15,9 @@ public:
      * @param polyhedron The polyhedron to create particle on
      * @param polyhedron_face The polyhedron's face to create particle on
      * @param coordinates The coordinates to create particle at
+     * @param angle Initial direction of the particle
      */
-    __device__ Particle(const Polyhedron *const polyhedron, int polyhedron_face, SpacePoint coordinates);
+    __device__ Particle(const Polyhedron *const polyhedron, int polyhedron_face, SpacePoint coordinates, int angle);
 
     /// The particle's location
     SpacePoint coordinates;
@@ -24,8 +25,27 @@ public:
     /// The corresponding sensor's location
     SpacePoint left_sensor, middle_sensor, right_sensor;
 
-    /// Normal vector to the current face
-    SpacePoint polyhedron_face_normal;
+    /// A number of the current face
+    int polyhedron_face;
+
+    /// A normal to the current face
+    SpacePoint normal;
+
+private:
+    /**
+     * Rotates given point at the angle relative to agent's coordinates
+     *
+     * @param radius Vector from agent to point to rotate
+     * @param angle Rotation angle
+     *
+     * @returns New coordinates of the point
+     */
+    __device__ SpacePoint rotate_point_angle(SpacePoint radius, int angle);
+
+    /// Initializes left and right sensors relative to the middle sensor
+    __device__ void init_left_right_sensors();
+
+
 };
 
 #endif //MIND_S_CRAWL_PARTICLE_CUH
