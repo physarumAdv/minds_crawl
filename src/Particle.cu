8ef9dec847222b34ff53781678e8c76f64197c41
tanya-kta <tanya-kta@bk.ru>
2020-05-20 23:22:43 +0300

    Fixed grammar & spelling

--- src/Particle.cu
+++ src/Particle.cu
@@ -10,12 +10,12 @@ __device__ Particle::Particle(const Polyhedron *const polyhedron, int polyhedron
     this->coordinates = coordinates;
     this->polyhedron_face = polyhedron_face;
 
-    Face current_face = polyhedron.get_face(polyhedron_face);
+    Face current_face = polyhedron->faces[polyhedron_face];
     this->normal = current_face.normal;
 
-    SpacePoint radius = polyhedron.get_vertex(current_face.vertices[0]) - coordinates;
+    SpacePoint radius = polyhedron->vertices[current_face.vertices[0]] - coordinates;
     radius = radius * jc::so / get_distance(radius, origin);
-    this->miggle_sensor = this.rotate_point_angle(radius, angle);
+    this->middle_sensor = this->rotate_point_angle(radius, angle);
 }
 
 __device__ SpacePoint Particle::rotate_point_angle(SpacePoint radius, int angle)
@@ -27,8 +27,8 @@ __device__ SpacePoint Particle::rotate_point_angle(SpacePoint radius, int angle)
 
 __device__ void Particle::init_left_right_sensors()
 {
-    this->left_sensor = this.rotate_point_angle(this->middle_sensor - this->coordinates, jc::sa);
-    this->right_sensor = this.rotate_point_angle(this->middle_sensor - this->coordinates, -jc::sa);
+    this->left_sensor = this->rotate_point_angle(this->middle_sensor - this->coordinates, jc::sa);
+    this->right_sensor = this->rotate_point_angle(this->middle_sensor - this->coordinates, -jc::sa);
     if(this->normal * ((this->right_sensor - this->coordinates) % (this->left_sensor - this->coordinates)) < 0)
     {
         SpacePoint p = this->right_sensor;
