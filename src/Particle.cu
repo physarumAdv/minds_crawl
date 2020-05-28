41e768996170b21ff26ad4b22e9d920a507c42dc
tanya-kta <tanya-kta@bk.ru>
2020-05-28 17:18:15 +0300

    Restructured `Particle`; defined `get_projected_vector_end`

--- src/Particle.cu
+++ src/Particle.cu
@@ -1,5 +1,4 @@
 #include "Particle.cuh"
-#include "Face.cuh"
 #include "fucking_shit.cuh"
 #include "jones_constants.hpp"
 
@@ -13,9 +12,8 @@ __device__ Particle::Particle(MapNode *map_node, SpacePoint coordinates, double 
     this->normal = current_face.normal;
 
     SpacePoint radius = current_face.vertices[0] - coordinates;
-    radius = radius * jc::so / get_distance(radius, origin);
-    this->middle_sensor = this->rotate_point_angle(radius, angle);
-    this->init_left_right_sensors();
+    radius = radius * jc::speed / get_distance(radius, origin);
+    this->direction_vector = this->rotate_point_angle(radius, angle);
 }
 
 
@@ -24,18 +22,6 @@ __device__ SpacePoint Particle::rotate_point_angle(SpacePoint radius, double ang
     double angle_cos = cos(angle);
     return (1 - angle_cos) * (this->normal * radius) * this->normal + angle_cos * radius +
                       sin(angle) * (this->normal % radius) + this->coordinates;
-}
-
-__device__ void Particle::init_left_right_sensors()
-{
-    this->left_sensor = this->rotate_point_angle(this->middle_sensor - this->coordinates, jc::sa);
-    this->right_sensor = this->rotate_point_angle(this->middle_sensor - this->coordinates, -jc::sa);
-    if(this->normal * ((this->right_sensor - this->coordinates) % (this->left_sensor - this->coordinates)) < 0)
-    {
-        SpacePoint p = this->right_sensor;
-        this->right_sensor = this->left_sensor;
-        this->left_sensor = p;
-    }
 }
 
 
@@ -71,4 +57,52 @@ __device__ void Particle::rotate(double angle)
     left_sensor = get_projected_vector_end(c, rotate_point_angle(left_sensor, angle), f, p);
     middle_sensor = get_projected_vector_end(c, rotate_point_angle(middle_sensor, angle), f, p);
     right_sensor = get_projected_vector_end(c, rotate_point_angle(right_sensor, angle), f, p);
+}
+
+__device__ SpacePoint get_projected_vector_end(SpacePoint a, SpacePoint b, int current_face_id, Polyhedron *polyhedron)
+{
+    Face current_face = polyhedron->faces[current_face_id];
+    for (int i = 0; i < current_face.n_of_vertices - 1; ++i)
+    {
+        SpacePoint intersection = line_intersection(current_face.vertices[i], current_face.vertices[i + 1], a, b);
+        if (intersection != origin && is_in_segment(a, b, intersection) &&
+            is_in_segment(current_face.vertices[i], current_face.vertices[i + 1], intersection) &&
+            get_distance(intersection, a) > eps)
+        {
+            Face next_face = polyhedron->faces[find_face_next_to_edge(a, b, current_face_id, polyhedron)];
+            SpacePoint normal_before = current_face.normal;
+            SpacePoint normal_after = next_face.normal;
+            SpacePoint moving_vector = (b - a) / get_distance(a, b);
+            double phi_cos = normal_after * normal_before;
+            double phi_sin = sin(acos(phi_cos));
+            double alpha_cos = moving_vector * (normal_before % normal_after);
+
+            SpacePoint faced_vector_direction = (normal_before + normal_after * phi_cos) * sin(acos(alpha_cos)) / phi_sin +
+                    (normal_before % normal_after) * alpha_cos / phi_sin;
+
+            return intersection + faced_vector_direction * (get_distance(a, b) - get_distance(intersection, a));
+        }
+    }
+    return b;
+}
+
+__device__ int find_face_next_to_edge(SpacePoint a, SpacePoint b, int current_face_id, Polyhedron *polyhedron)
+{
+    for(int i = 0; i < polyhedron->n_of_faces; ++i)
+        if (polyhedron->faces[i].id != current_face_id && is_edge_belongs_face(a, b, polyhedron->faces[i]))
+            return i;
+    return current_face_id;
+}
+
+__device__ bool is_edge_belongs_face(SpacePoint a, SpacePoint b, const Face& face)
+{
+    bool flag1 = false, flag2 = false;
+    for (int i = 0; i < face.n_of_vertices; ++i)
+    {
+        if (face.vertices[i] == a)
+            flag1 = true;
+        if (face.vertices[i] == b)
+            flag2 = true;
+    }
+    return flag1 && flag2;
 }
