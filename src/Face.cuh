9f86617f28f5e42a5509337dcc1c1952ab1db2aa
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2020-05-28 16:05:24 +0300

    Added docs and deleted copy constructor for `Face`

--- src/Face.cuh
+++ src/Face.cuh
@@ -25,11 +25,20 @@ public:
      * Creates a `Face` object
      *
      * @param id Identifier of the polyhedron face
-     * @param vertices Array of polyhedron vertices that belong to the face
+     * @param vertices Array of polyhedron vertices that belong to the face.
+     *      Must be ordered in a special way (see note below)
      * @param n_of_vertices Number of vertices on the face
      * @param node Some node laying on the face
+     *
+     * @note The vertices order. Looking on a face <b>from outside</b> the polyhedron, some vertex (let's call it A)
+     * must be saved to vertices[0]. It's neighbour clockwise - vertex B (B is next to A clockwise) must be saved to
+     * vertices[1] and so on. Assuming there are N vertices in total, A's neighbors are B clockwise and
+     * X counterclock-wise, X must be saved to vertices[N - 2], and A must be saved <b>again</b> to vertices[N - 1]
      */
     __device__ Face(int id, const SpacePoint *vertices, ll n_of_vertices, MapNode *node);
+
+    /// Forbids copying `Face` objects
+    __host__ __device__ Face(const Face &) = delete;
 
     /// Destructs a `Face` object
     __device__ ~Face();
@@ -38,8 +47,10 @@ public:
     /// An identifier of the face of a `Polyhedron`
     const int id;
 
-    /// Array of vertices that belong to the face
-    const SpacePoint *vertices;
+    /**
+     * Array of vertices that belong to the face (represented as described in the constructor)
+     */
+    const SpacePoint *const vertices;
 
     /// Number of vertices on the face
     const ll n_of_vertices;
