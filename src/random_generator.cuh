614b79149d2d7d88179f7e6e5b359c6af7637920
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2020-08-27 22:39:34 +0300

    Rearranged code in random generator, made `rand0to1` `__host__ __device__`

--- src/random_generator.cuh
+++ src/random_generator.cuh
@@ -2,6 +2,9 @@
 #define MIND_S_CRAWL_RANDOM_GENERATOR_CUH
 
 
+#ifndef COMPILE_FOR_CPU
+
+
 /**
  * Initializes cuRAND
  *
@@ -14,14 +17,19 @@
  */
 __global__ void init_rand(unsigned long long seed);
 
+
+#endif //COMPILE_FOR_CPU
+
+
 /**
  * Generates a pseudo-random `double` in range [0; 1]
  *
  * @returns A pseudo-random `double` in range [0; 1]
  *
- * @warning To use this function you firstly need to initialize random via `init_rand`
+ * @warning To use this function <b>from device code</b> you must firstly initialize random via `init_rand`. However, if
+ *      you are calling this function from host code, you don't have to initialize it
  */
-__device__ double rand0to1();
+__host__ __device__ double rand0to1();
 
 
 #endif //MIND_S_CRAWL_RANDOM_GENERATOR_CUH
