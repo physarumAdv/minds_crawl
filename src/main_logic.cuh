8b5c76cbdc84c8ca8a514d35288034039a99fa83
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2020-09-22 10:28:35 +0300

    Documented functions related to integration with visualization
    Added documentation for `get_visualization_endpoint`, `vector_double_to_json_array`, `send_particles_to_visualization`

--- src/main_logic.cuh
+++ src/main_logic.cuh
@@ -260,6 +260,13 @@ __device__ inline void run_iteration_cleanup(SimulationMap *const simulation_map
 }
 
 
+/**
+ * Reads the first line from the file containing the visualization endpoint url and returns it
+ *
+ * Path to the file (relative to the executable's location) is `local/visualization_endpoint.txt`
+ *
+ * @returns Url to send data to be visualized to
+ */
 __host__ std::string get_visualization_endpoint()
 {
     std::ifstream f("local/visualization_endpoint.txt", std::ios::in);
@@ -268,6 +275,15 @@ __host__ std::string get_visualization_endpoint()
     return url;
 }
 
+/**
+ * Converts a `vector` of `double` to a JSON array
+ *
+ * `std::to_string(double)` is used to create array elements
+ *
+ * @param v Array to be converted to JSON
+ *
+ * @returns `std::string` with JSON array of numbers with floating point
+ */
 __host__ std::string vector_double_to_json_array(const std::vector<double> &v)
 {
     if(v.empty())
@@ -283,6 +299,29 @@ __host__ std::string vector_double_to_json_array(const std::vector<double> &v)
     return ans;
 }
 
+/**
+ * Send information about the particles of a simulation to visualization appliction
+ *
+ * The information about the existing particles is collected from the `nodes` array. Their coordinates are extracted.
+ * The following JSON is generated: `{"x": [x coordinates of the particles (double)], "y": [y coordinates], "z": [z
+ * coordinates]}`. For example, to represent two particles with coordinates (0, 0, 0) and (0.5, 1, 2), the following
+ * JSON is generated: `{"x": [0.000000, 0.500000], "y": [0.000000, 1.000000], "z": [0.000000, 2.000000]}`. Then it is
+ * sent to the given url as an HTTP POST request with `Content-Type: application/json`
+ *
+ * @param url HTTP url to send data to (protocols different from http are not supported)
+ * @param nodes Pointer-represented array of nodes from the simulation (probably) containing particles
+ * @param n_of_nodes Number of elements in the `nodes` array
+ *
+ * @returns `true` if the request was successfully sent, `false` if there was an exception from the http lib (it will
+ *      be caught and printed to stderr)
+ *
+ * @note This function was not designed to be used in various contexts different from the simulation's main flow, were
+ *      it's needed to send the data to visualization and only know, whether it was sent successfully or not (the
+ *      end-user will see the error in stderr if it exists, but the function caller won't be able to handle it, because
+ *      it's handled inside of `send_particles_to_visualization`). The reason for this decision is to reduce code
+ *      duplication (because there are two main functions for cpp and cuda), which might mean for you that you don't
+ *      want to use this function but want to write your own request sender
+ */
 __host__ bool send_particles_to_visualization(const std::string &url, MapNode *nodes, int n_of_nodes)
 {
     std::vector<double> x, y, z;
